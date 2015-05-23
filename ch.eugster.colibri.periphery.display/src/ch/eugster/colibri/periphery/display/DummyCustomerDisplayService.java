1f2aa11738f3242b2f2bcfbd091c6ec90f77baa5
Christian Eugster <christian.eugster@gmx.net>
2015-05-23 09:26:08 +0000

    New dummy display service for test purposes with service-component.xml for diynamic starting of service.

---
+++ ch.eugster.colibri.periphery.display/src/ch/eugster/colibri/periphery/display/DummyCustomerDisplayService.java
@@ -0,0 +1,57 @@
+package ch.eugster.colibri.periphery.display;
+
+import ch.eugster.colibri.periphery.converters.Converter;
+import ch.eugster.colibri.periphery.display.service.AbstractCustomerDisplayService;
+
+public class DummyCustomerDisplayService extends AbstractCustomerDisplayService {
+
+	@Override
+	public void clearDisplay() 
+	{
+		System.out.println();
+	}
+
+	@Override
+	public void displayText(int timerDelay, String text) 
+	{
+		try
+		{
+			Thread.sleep(1000 * timerDelay);
+		}
+		catch (InterruptedException e)
+		{
+		}
+		displayText(text);
+	}
+
+	@Override
+	public void displayText(String text) 
+	{
+		if (text.length() > this.getColumnCount())
+		{
+			System.out.println(text.substring(0,this.getColumnCount()));
+			System.out.println(text.substring(this.getColumnCount()));
+			System.out.println();
+		}
+		else
+		{
+			System.out.println(text);
+			System.out.println();
+			System.out.println();
+		}
+	}
+
+	@Override
+	public void displayText(String converter, String text) 
+	{
+		displayText(text);
+	}
+
+	@Override
+	public void testDisplay(String deviceName, String conversions, String text)
+			throws Exception 
+	{
+		displayText(text);
+	}
+
+}
