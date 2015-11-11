402b943962f09f3aa95f8325e8a5fd5ab1765a06
Christian Eugster <christian.eugster@gmx.net>
2015-11-11 12:56:07 +0000

    added count details to DayTimeRow

--- ch.eugster.colibri.persistence/src/ch/eugster/colibri/persistence/model/DayTimeRow.java
+++ ch.eugster.colibri.persistence/src/ch/eugster/colibri/persistence/model/DayTimeRow.java
@@ -9,13 +9,22 @@ public class DayTimeRow extends HashMap<Object, Object>
 	 */
 	private static final long serialVersionUID = -7085652736575375292L;
 
-	public DayTimeRow(Long salespointId, String name, Integer hour, Double amount, int receipts)
+	public DayTimeRow(Long salespointId, String name, Integer hour, Double amount, Integer count)
 	{
+		if (amount == null)
+		{
+			amount = new Double(0D);
+		}
+		if (count == null)
+		{
+			count = new Integer(0);
+		}
 		this.put("id", salespointId);
 		this.put("name", name);
-		this.put("h" + hour.toString(), amount);
-		this.put("total", amount);
-		this.put("receipts", receipts);
+		this.put("amount@" + hour.toString(), amount);
+		this.put("amount", amount);
+		this.put("count@" + hour.toString(), count);
+		this.put("count", count);
 	}
 	
 	public Object get(String key)
@@ -23,15 +32,30 @@ public class DayTimeRow extends HashMap<Object, Object>
 		return super.get(key);
 	}
 	
-	public void add(Integer hour, Double amount)
+	public void add(Integer hour, Double amount, Integer count)
 	{
-		Double oldAmount = (Double) this.get("h" + hour.toString());
-		Double newAmount = 0D;
-		newAmount = oldAmount == null ? amount : new Double(oldAmount.doubleValue() + amount.doubleValue());
-		this.put("h" + hour.toString(), newAmount);
-		Double total = (Double) this.get("total");
-		total = total == null ? newAmount : new Double(total.doubleValue() + newAmount.doubleValue());
-		this.put("total", total);
-		this.put("average", total / (int)this.get("receipts"));
+		amount = getAmount(amount);
+		Double oldAmount = getAmount((Double) this.get("amount@" + hour.toString()));
+		Double newAmount = new Double(oldAmount.doubleValue() + amount.doubleValue());
+		this.put("amount@" + hour.toString(), newAmount);
+		oldAmount = getAmount((Double) this.get("amount"));
+		this.put("amount", new Double(oldAmount.doubleValue() + amount.doubleValue()));
+		
+		count = getCount(count);
+		Integer oldCount = getCount((Integer) this.get("count@" + hour.toString()));
+		Integer newCount = new Integer(oldCount.intValue() + count.intValue());
+		this.put("count@" + hour.toString(), newCount);
+		oldCount = getCount((Integer) this.get("count"));
+		this.put("count", oldCount + count);
+	}
+	
+	private Double getAmount(Double amount)
+	{
+		return amount == null ? new Double(0D) : amount;
+	}
+
+	private Integer getCount(Integer count)
+	{
+		return count == null ? new Integer(0) : count;
 	}
 }
